e52dfdb9a71c01535ab2064fc449f1dcab925bca
Johnson Dubula <jdubula@iqbusiness.net>
2022-06-27 22:00:48 +0200

    Ejs set up

--- nodeProject/server.js
+++ nodeProject/server.js
@@ -2,9 +2,10 @@ console.log("hey ")
 const express = require('express');
 const app = express();
 
-
+app.set('view engine', 'ejs')
 app.get("/", (req, res) => {
-    res.send("hello jam at 4000");
+    //  res.send("hello jam at 4000");
+    res.render("index")
 })
 
 
