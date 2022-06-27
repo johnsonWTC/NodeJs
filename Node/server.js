10d4c99db1d46881f2eb56360ce9c53dcb799e5d
Johnson Dubula <jdubula@iqbusiness.net>
2022-06-28 00:02:00 +0200

    routing

--- Node/server.js
+++ Node/server.js
@@ -7,7 +7,15 @@ app.set('view engine', 'ejs')
 console.log("hello jam");
 
 app.get('/', (req, res) => {
-    res.send("hello again");
+    res.render("index", { text: "Johnson" });
+})
+
+app.get('/users', (req, res) => {
+    res.send('user lists');
+})
+
+app.get("/users/new", (req, res) => {
+    res.send('new user form')
 })
 
 app.listen('200');
