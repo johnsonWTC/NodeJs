35f1f1e8477217489ec917ff41aa3f3ce670c208
Johnson Dubula <jdubula@iqbusiness.net>
2022-06-27 22:58:59 +0200

    sending text from backend to front end

--- nodeProject/server.js
+++ nodeProject/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'ejs')
 app.get("/", (req, res) => {
     //  res.send("hello jam at 4000");
-    res.render("index")
+    res.render("index", { text: "Man" })
 })
 
 
