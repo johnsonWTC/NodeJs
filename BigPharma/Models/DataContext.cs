f5479c323ec84d0bb5c8c49ff1b5cf48056dfe11
Johnson Dubula <jdubula@iqbusiness.net>
2022-07-10 15:51:41 +0200

    init

---
+++ BigPharma/Models/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BigPharma.Models
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+          
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    // connect to sql server with connection string from app settings
+        //    options.UseSqlServer(Configuration.GetConnectionString("NewDb"));
+        //}
+
+        public DbSet<User> Users { get; set; }
+    }
+    }
